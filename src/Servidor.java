fc5aa41292d6a38fc6d4135cf01ccb8b19b534df
Ivan <ivanbartej11@gmail.com>
2025-12-08 20:06:32 +0100

    cuarta parte, con la implementación del REGISTER

--- src/Servidor.java
+++ src/Servidor.java
@@ -20,26 +20,26 @@ public class Servidor {
         BufferedWriter salida = new BufferedWriter(new OutputStreamWriter(cliente.getOutputStream()));
 
         // Seleccionamos el fichero en el que se encuentran los datos y creamos un Buffer para leer los datos de este
-        File fichero = new File("out/production/dns-simplificado-Ivan-Barreno/TodosLosRegistros");
+        File fichero = new File("src/TodosLosRegistros");
         BufferedReader br = new BufferedReader(new FileReader(fichero));
 
         // Creamos un HashMap que tenga como key: el dominio y como valor: ArrayList<Registros> ya que un dominio como google puede tener varios valores
         HashMap<String, ArrayList<Registro>> registros = new HashMap<>();
-        String linea;
 
         // Rellenamos el HasMap con los datos del txt, el formato de cada línea es: google.com A 172.217.17.4
+        String linea;
         while ((linea = br.readLine()) != null) {
             String[] partes = linea.trim().split(" ");
 
             String dominio = partes[0];
             String tipo = partes[1];
             String ip = partes[2];
-
             Registro registro = new Registro(dominio, tipo, ip);
+
 
             // Creamos una entrada si no existe al valor que estamos mirando actualmente. putIfAbsent es perfecto para esto. (El ArrayList que se crea está vacio luego se rellena)
             registros.putIfAbsent(dominio, new ArrayList<>());
-            // Vamos a la entrada a la que queremos añadir el registro y lo ponemos.
+            // Vamos a la entrada a la que queremos añadir el registro y lo ponemos. (En este caso cojemos un ArrayList acordarse)
             registros.get(dominio).add(registro);
         }
 
@@ -84,7 +84,7 @@ public class Servidor {
                         if (mensajeCliente.split(" ").length != 1) {
                             salida.write("400 Bad request\n");
                             salida.flush();
-                        } else{
+                        } else {
                             String respuesta = respuestaList(registros);
 
                             for (String r : respuesta.split("\n")) {
@@ -93,7 +93,18 @@ public class Servidor {
                             }
                         }
                         salida.flush();
+                    }
 
+                    case "REGISTER" -> {
+                        if (mensajeCliente.split(" ").length != 4) {
+                            salida.write("400 Bad request\n");
+                            salida.flush();
+                        } else {
+                            String[] parte = mensajeCliente.split(" ");
+                            String respuesta = respuestaRegister(parte[1], parte[2], parte[3], fichero, registros);
+                            salida.write(respuesta + "\n");
+                            salida.flush();
+                        }
                     }
 
                     default -> {
@@ -143,13 +154,50 @@ public class Servidor {
         return resultado.toString().trim();
     }
 
+    private static String respuestaRegister(String dominio, String tipo, String valor, File fichero, HashMap<String, ArrayList<Registro>> registros) {
+            /*
+            Hacer comprobaciones para saber si está correctamente escrito, uso de expresiones regulares (regex)
+
+            Validación del dominio:
+            - [a-zA-Z0-9-]+ -> letras, números o guion, al menos uno
+            - [.]           -> un punto literal
+            - [a-zA-Z]+     -> solo letras, al menos una
+             */
+
+        if (!dominio.matches("[a-zA-Z0-9-]+[.]{1}[a-zA-Z]+")) return ("400 Bad request\n");
+
+        if (tipo.equals("A")) {
+            // Valor no vacío y contiene solo dígitos y puntos
+            if (!valor.matches("[0-9.]+")) return "400 Bad request\n";
+
+        } else if (tipo.equals("MX") || tipo.equals("CNAME")) {
+            // Valor no vacío y contiene letras o guiones o puntos
+            if (!valor.matches("[a-zA-Z0-9-.]+")) return "400 Bad request\n";
+
+        } else {
+            // Tipo no válido
+            return "400 Bad request\n";
+        }
+
+        // Una vez está todo correcto añadimos al HasMap y al txt
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fichero, true))) {
+            Registro nuevo = new Registro(dominio, tipo, valor);
+            registros.putIfAbsent(dominio, new ArrayList<>());
+            registros.get(dominio).add(nuevo);
+
+            bw.write(dominio + " " + tipo + " " + valor + "\n");
+            return "200 Record added";
+        } catch (IOException e) {
+            return "500 Error de servidor";
+        }
+    }
 
     private static String respuestaList(HashMap<String, ArrayList<Registro>> registros) {
         StringBuilder respuesta = new StringBuilder();
 
         respuesta.append("150 Inicio listado\n");
 
-        for (Map.Entry<String, ArrayList<Registro>> entry : registros.entrySet()) { // registros.entry devuelve cada entrada
+        for (Map.Entry<String, ArrayList<Registro>> entry : registros.entrySet()) { // por cada entrada entry, recorre todos los valores del map
             ArrayList<Registro> listaActual = entry.getValue();
 
             for (Registro registroActual : listaActual) {
